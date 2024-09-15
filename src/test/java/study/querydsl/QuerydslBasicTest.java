a86c1a3a76addc6d15d95c90d92d9dc6b3ccfe3e
uzz99 <yj.lee991228@gmail.com>
2024-09-16 00:11:47 +0900

    test: tupleProjecion

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -470,4 +470,19 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void tupleProjecion() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for(Tuple tuple : result){
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
+
 }
